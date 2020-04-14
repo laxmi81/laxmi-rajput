4d649fc56f08e115c5bfdbe70148b0a6c225bf02
laxmi81 <l81rajput@gmail.com>
2020-04-13 20:46:53 -0700

    commit both pro1 and  pro2

---
+++ src/git_demo/pro1.java
@@ -0,0 +1,11 @@
+package git_demo;
+
+public class pro1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("this is main master program"); 
+System.out.println("second change");
+	}
+
+}
---
+++ src/git_demo/pro2.java
@@ -0,0 +1,11 @@
+package git_demo;
+
+public class pro2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("this is second command");
+System.out.println("thios is pro2");
+	}
+
+}
